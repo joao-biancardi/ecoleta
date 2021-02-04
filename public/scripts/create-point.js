e991cebd65b76d36c7f0a0c41a7c2215bb3f92a5
joao-biancardi <57998394+joao-biancardi@users.noreply.github.com>
2021-02-04 15:49:51 -0300

    first commit

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -36,10 +36,7 @@ function getCities(event) {
         })
 }
 
-document
-    .querySelector("select[name=uf]")
-    .addEventListener("change", getCities)
-
+document.querySelector("select[name=uf]").addEventListener("change", getCities)
 
 //Itens de coleta
 const itemsToCollect = document.querySelectorAll(".items-grid li")
